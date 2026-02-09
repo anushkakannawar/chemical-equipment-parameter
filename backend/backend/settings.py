0df10db4937513426fc1579b860438e2d439c518
Anushka Kannawar <anushkakannawar8@gmail.com>
2026-02-10 00:04:12 +0530

    changed settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -105,4 +105,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://chemical-equipment-parameter-zw76.onrender.com"
 ]
